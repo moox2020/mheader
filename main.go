52e1e58cb70da8985490bd3a8ab06bc9810be888
moox2020 <1591112944@qq.com>
2024-03-14 15:14:58 +0800

    add version

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import "net/http"
 type AddHeader struct {
 }
 
+// 假设每个package中必须使用New函数返回
 func New() http.Handler {
 	return &AddHeader{}
 }
 
 func (this *AddHeader) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Add("name", "mheader--v2")
+	writer.Header().Add("name", "mheader--v3")
 }
