52b71d4ce27ec8bc655f7e4ba35784bb7b5436b0
moox2020 <1591112944@qq.com>
2024-03-14 11:07:35 +0800

    update main.go package

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
-package main
+package mheader
 
-import "fmt"
+import "net/http"
 
-func main() {
-	fmt.Println("hello world.")
+type AddHeader struct {
+}
+
+func New() http.Handler {
+	return &AddHeader{}
+}
+
+func (this *AddHeader) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("name", "mheader--v2")
 }
